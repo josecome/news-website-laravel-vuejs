acb990e6732fcec97e22e8641466e75e0f1769d9
Jose Jaime Come <josejaimecome@gmail.com>
2023-09-03 14:07:52 +0200

    Minor changes made

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,13 +9,21 @@ class ApiController extends Controller
 {
     public function home()
     {
+        $func_mth_yr = function($value) {
+            $values = explode("-", $value);
+            return $values[1] . "-" . $values[0];
+        };
         $breaking_news = 0;
-        $featured_section = 1;
+        $featured_section = [1, 2];
         $news = News::orderBy('news_date', 'desc')->get(['id', 'title', 'content', 'news_date']);
+        $news_month_year = News::orderBy('news_date', 'desc')->pluck('news_date')->toArray();
+        $news_months_year = array_map($func_mth_yr, $news_month_year );
 
         return response()->json([
             'news' => $news,
-            'breaking_news' => $breaking_news
+            'breaking_news' => $breaking_news,
+            'news_month_year' => $news_months_year,
+            'featured_section' => $featured_section,
         ]);
     }
     public function NewsById($id)
--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,14 @@ use Inertia\Inertia;
 |
 */
 
-/* Route::get('/', function () {
+/*Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-}); */
+});*/
 Route::get('/', [NewsController::class, 'home']);
 Route::get('/news/{id}', [NewsController::class, 'NewsById']);
 
