74edc23d6004b21e31622fb43135bb7fe57359d2
Jose Jaime Come <josejaimecome@gmail.com>
2023-09-03 17:35:40 +0200

    Content added to News Page by id

--- resources/js/Pages/News.vue
+++ resources/js/Pages/News.vue
@@ -1,3 +1,22 @@
+<script setup>
+import AppHead from '../Components/Head.vue'
+defineProps({
+    news: {
+        type: Object,
+    },
+});
+</script>
 <template>
-    Testing...
+    <AppHead title="News Web Page" />
+    <div id="content">
+      <h1 class="display-4 fst-italic">{{ news[0].title }}</h1>
+      <p class="lead my-3">{{ news[0].content }}</p>
+    </div>
 </template>
+<style scoped>
+#content {
+    width: 80%;
+    margin: auto;
+    padding: 10px;
+}
+</style>
