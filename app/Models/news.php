ad64b34b5bce55f043eae85e7fdb77197c6d9b09
Jose Jaime Come <josejaimecome@gmail.com>
2023-11-20 21:01:09 +0200

    News and Comment models updated

--- app/Models/news.php
+++ app/Models/news.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class News extends Model
 {
@@ -13,5 +14,9 @@ class News extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+    public function likes(): MorphMany
+    {
+        return $this->morphMany(Like::class, 'likeable');
     }
 }
